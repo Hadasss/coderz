8783c9312b35b2a370d5fef957de3f996484fb8d
Hadasss <hadas.gadish@gmail.com>
2022-01-21 14:38:15 -0700

    set time for quiz

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,7 +19,7 @@ if (localStorage.getItem("scores")) {
 }
 let currScore = "";
 let countdown = "";
-let counter = 30;
+let counter = 110;
 let questionIndex = 0;
 const questionsArr = [
   {
@@ -38,84 +38,84 @@ const questionsArr = [
     answer: "Both are equal in the value and data type",
   },
 
-  //   {
-  //     questionText: "In JavaScript, what is a block of statement?",
-  //     choices: [
-  //       "Conditional block",
-  //       "block that combines a number of statements into a single compound statement",
-  //       "both conditional block and a single statement",
-  //       "block that contains a single statement",
-  //     ],
-  //     answer:
-  //       "block that combines a number of statements into a single compound statement",
-  //   },
+  {
+    questionText: "In JavaScript, what is a block of statement?",
+    choices: [
+      "Conditional block",
+      "block that combines a number of statements into a single compound statement",
+      "both conditional block and a single statement",
+      "block that contains a single statement",
+    ],
+    answer:
+      "block that combines a number of statements into a single compound statement",
+  },
 
-  //   {
-  //     questionText:
-  //       "When interpreter encounters an empty statements, what it will do:",
-  //     choices: [
-  //       "Shows a warning",
-  //       "Prompts to complete the statement",
-  //       "Throws an error",
-  //       "Ignores the statements",
-  //     ],
-  //     answer: "Ignores the statements",
-  //   },
+  {
+    questionText:
+      "When interpreter encounters an empty statements, what it will do:",
+    choices: [
+      "Shows a warning",
+      "Prompts to complete the statement",
+      "Throws an error",
+      "Ignores the statements",
+    ],
+    answer: "Ignores the statements",
+  },
 
-  //   {
-  //     questionText: "The 'function' and 'var' are known as:",
-  //     choices: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
-  //     answer: "Declaration statements",
-  //   },
+  {
+    questionText: "The 'function' and 'var' are known as:",
+    choices: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
+    answer: "Declaration statements",
+  },
 
-  //   {
-  //     questionText:
-  //       "Which of the following variables takes precedence over the others if the names are the same?",
-  //     choices: [
-  //       "Global variable",
-  //       "The local element",
-  //       "The two of the above",
-  //       "None of the above",
-  //     ],
-  //     answer: "The local element",
-  //   },
+  {
+    questionText:
+      "Which of the following variables takes precedence over the others if the names are the same?",
+    choices: [
+      "Global variable",
+      "The local element",
+      "The two of the above",
+      "None of the above",
+    ],
+    answer: "The local element",
+  },
 
-  //   {
-  //     questionText:
-  //       "Which one of the following is the correct way for calling the JavaScript code?",
-  //     choices: ["Preprocessor", "Triggering Event", "RMI", "Function/Method"],
-  //     answer: "Function/Method",
-  //   },
+  {
+    questionText:
+      "Which one of the following is the correct way for calling the JavaScript code?",
+    choices: ["Preprocessor", "Triggering Event", "RMI", "Function/Method"],
+    answer: "Function/Method",
+  },
 
-  //   {
-  //     questionText: "Which of the following type of a variable is volatile?",
-  //     choices: [
-  //       "Mutable variable",
-  //       "Dynamic variable",
-  //       "Volatile variable",
-  //       "Immutable variable",
-  //     ],
-  //     answer: "Mutable variable",
-  //   },
+  {
+    questionText: "Which of the following type of a variable is volatile?",
+    choices: [
+      "Mutable variable",
+      "Dynamic variable",
+      "Volatile variable",
+      "Immutable variable",
+    ],
+    answer: "Mutable variable",
+  },
 
-  //   {
-  //     questionText:
-  //       "Which of the following number object function returns the value of the number?",
-  //     choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
-  //     answer: "valueOf()",
-  //   },
+  {
+    questionText:
+      "Which of the following number object function returns the value of the number?",
+    choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+    answer: "valueOf()",
+  },
 
-  //   {
-  //     questionText:
-  //       "In the JavaScript, which one of the following is not considered as an error:",
-  //     choices: [
-  //       "Syntax error",
-  //       "Missing of semicolons",
-  //       "Division by zero",
-  //       "Missing of Bracket",
-  //     ],
-  //     answer: "Division by zero",
-  //   },
+  {
+    questionText:
+      "In the JavaScript, which one of the following is not considered as an error:",
+    choices: [
+      "Syntax error",
+      "Missing of semicolons",
+      "Division by zero",
+      "Missing of Bracket",
+    ],
+    answer: "Division by zero",
+  },
 ];
 let questionTitle = document.querySelector(".question");
 let currQuestion;
